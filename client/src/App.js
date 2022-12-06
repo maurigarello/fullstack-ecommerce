df09b58f1a85c9bfb5e31d28b3a7ad78c508e48a
Mauricio Garello <mauriciogarellog2g@gmail.com>
2022-12-06 12:58:08 -0300

    commit inicial

---
+++ client/src/App.js
@@ -0,0 +1,40 @@
+import { useEffect } from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useLocation
+} from 'react-router-dom';
+import Checkout from './scenes/checkout/Checkout.jsx';
+import Confirmation from './scenes/checkout/Confirmation.jsx';
+import Navbar from './scenes/global/Navbar.jsx';
+import Home from "./scenes/home/Home.jsx";
+import ItemDetails from './scenes/itemDetails/ItemDetails.jsx';
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() =>{  
+    window.scrollTo(0, 0);
+  }, [pathname])
+
+  return null;
+}
+
+
+function App() {
+  return (
+      <BrowserRouter>
+      <Navbar />
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="item/:itemId" element={<ItemDetails />} />
+        <Route path="checkout" element={<Checkout />} />
+        <Route path="checkout/succes" element={<Confirmation />} />
+      </Routes>
+      </BrowserRouter>
+  );
+}
+
+export default App;
